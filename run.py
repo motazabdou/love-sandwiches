e9cfb283b9dd39e65a6e5855ca23e7ab2a0cb69d
motazabdou <motazabdou30@gmail.com>
2021-08-20 13:25:43 +0000

    add while loop to repeat data request for as long as data entered is false, and break when data is entered in correct format

--- run.py
+++ run.py
@@ -14,13 +14,18 @@ SHEET = GSPREAD_CLIENT.open("love_sandwiches")
 
 def get_sales_data():
     """Get sales input figures from the user"""
-    print("Please enter sales data from the last market.")
-    print("Data should be six numbers, seperated by commas")
-    print("Example: 10,20,30,40,50,60\n")
+    while True:
+        print("Please enter sales data from the last market.")
+        print("Data should be six numbers, seperated by commas")
+        print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Please enter your data here in the format shown above: ")
-    sales_data = data_str.split(",")
-    validate_data(sales_data)
+        data_str = input("Please enter your data here in the format shown above: ")
+        sales_data = data_str.split(",")
+        if validate_data(sales_data):
+            print("Data entered is valid")
+            break
+
+    return sales_data
 
 
 def validate_data(values):
@@ -32,10 +37,14 @@ def validate_data(values):
     try:
         [int(value) for value in values]
         if len(values) != 6:
-            raise ValueError(f"Exactly 6 values are required. You provided {len(values)}")
+            raise ValueError(f"Exactly 6 values are required. You provided {len(values)}\n")
     
     except ValueError as e:
-        print(f"Invalid data {e}, please try again")
+        print(f"Invalid data {e}, please try again\n")
+        print("--------------------------------------\n")
+        return False
+
+    return True
 
 
-get_sales_data()
+data = get_sales_data()
